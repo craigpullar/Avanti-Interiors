507d4794ffa096e81042ac60872e7aaaa1317943
Craig Pullar <craigmichaelpullar@gmail.com>
2014-10-04 16:29:04 +0100

    added about us and header updates

--- landing_page/models.py
+++ landing_page/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# class Header(models.Model):
+# 	email = models.CharField(max_length = 250)
+# 	phone = models.CharField(max_length = 20)
+
+# class SlideShow(models.Model):
+# 	pass
+
+# class HomePage(models.Model):
+# 	about_us = models.TextField()
+# 	slideshow = models.OneToOneField(SlideShow)
+
+# class OfficePage(models.Model):
+# 	image1 = models.ImageField()
+# 	image2 = models.ImageField()
+# 	description = models.TextField()
+
+# class CeilingPage(models.Model):
+# 	image1 = models.ImageField()
+# 	image2 = models.ImageField()
+# 	description = models.TextField()
+
+# class OtherPage(models.Model):
+# 	pass
+
+# class Footer(models.Model):
+# 	pass
