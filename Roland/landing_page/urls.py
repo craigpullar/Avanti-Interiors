58966daae276f79f32982fba474ad55b74ad8bb3
Craig Pullar <craigmichaelpullar@gmail.com>
2015-01-05 15:21:01 +0000

    Added url for deafultsite

--- Roland/landing_page/urls.py
+++ Roland/landing_page/urls.py
@@ -3,6 +3,7 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$',views.index, name='home'),
+    url(r'^defaultsite$', views.index, name='home'),
     url(r'^partitioning$',views.partitioning, name='partitioning'),
     url(r'^ceilings$',views.ceilings, name='celings'),
     url(r'^other$',views.other, name='other'),
