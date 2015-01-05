45be6f22f6b458a045eeebcbb888d51d16d95653
Craig Pullar <craigmichaelpullar@gmail.com>
2015-01-05 15:25:57 +0000

    Added url for deafultsite

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -3,6 +3,7 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$',views.index, name='home'),
+    url(r'^defaultsite$',views.index, name='home'),
     url(r'^partitioning$',views.partitioning, name='partitioning'),
     url(r'^ceilings$',views.ceilings, name='celings'),
     url(r'^mezzanine$',views.mezzanine, name='mezzanine'),
